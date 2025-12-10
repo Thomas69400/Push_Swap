b66e0932d6437d73ceb573eae426215391f1bda6
Thomas69400 <bbstryllogy@gmail.com>
2025-12-10 14:30:04 +0100

    Refactor push function + WIP complex algo (working for ~50 numbers but not more)

--- reverse.c
+++ reverse.c
@@ -3,30 +3,30 @@
 /*                                                        :::      ::::::::   */
 /*   reverse.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tchemin <tchemin@student.42lyon.fr>        +#+  +:+       +#+        */
+/*   By: tchemin <tchemin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/06 11:14:03 by tchemin           #+#    #+#             */
-/*   Updated: 2025/12/09 19:41:50 by tchemin          ###   ########.fr       */
+/*   Updated: 2025/12/10 13:25:08 by tchemin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void	reverse(t_list **lst)
+void reverse(t_list **lst)
 {
-	t_list	*tmp;
+	t_list *tmp;
 
 	if (!lst || !*lst)
-		return ;
+		return;
 	ft_printf("rr%c\n", (*lst)->name);
 	tmp = last_lst(*lst);
 	tmp->prev->next = NULL;
 	add_front_lst(lst, tmp);
-	(*lst)->prev = tmp->prev;
-	*lst = tmp;
+	tmp->prev = NULL;
+	*lst = first_lst(*lst);
 }
 
-void	d_reverse(t_list **first_lst, t_list **second_lst)
+void d_reverse(t_list **first_lst, t_list **second_lst)
 {
 	reverse(first_lst);
 	reverse(second_lst);
--- rotate.c
+++ rotate.c
@@ -3,30 +3,31 @@
 /*                                                        :::      ::::::::   */
 /*   rotate.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tchemin <tchemin@student.42lyon.fr>        +#+  +:+       +#+        */
+/*   By: tchemin <tchemin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/06 10:49:38 by tchemin           #+#    #+#             */
-/*   Updated: 2025/12/09 19:41:44 by tchemin          ###   ########.fr       */
+/*   Updated: 2025/12/10 13:30:15 by tchemin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void	rotate(t_list **lst)
+void rotate(t_list **lst)
 {
-	t_list	*tmp;
+	t_list *tmp;
 
 	if (!lst || !*lst)
-		return ;
+		return;
 	ft_printf("r%c\n", (*lst)->name);
 	tmp = first_lst(*lst);
 	*lst = tmp->next;
+	tmp->next = NULL;
 	add_back_lst(lst, tmp);
 	(*lst)->prev = NULL;
-	tmp->next = NULL;
+	*lst = first_lst(*lst);
 }
 
-void	d_rotate(t_list **first_lst, t_list **second_lst)
+void d_rotate(t_list **first_lst, t_list **second_lst)
 {
 	rotate(first_lst);
 	rotate(second_lst);
