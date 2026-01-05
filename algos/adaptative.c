84080b103ffbc8d5cd1ccae8a33c93f924fd897d
Thomas Chemin <tchemin@z4r8p7.42lyon.fr>
2026-01-05 14:42:48 +0100

    buf on adaptative corrected

--- algos/adaptative.c
+++ algos/adaptative.c
@@ -6,7 +6,7 @@
 /*   By: tchemin <tchemin@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/20 13:26:42 by tchemin           #+#    #+#             */
-/*   Updated: 2025/12/26 13:55:24 by tchemin          ###   ########.fr       */
+/*   Updated: 2026/01/05 14:40:30 by tchemin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ t_list	*adaptative(t_list *lst_a, t_list *lst_b, t_bench *bench)
 
 	disorder = compute_disorder(lst_a) * 100;
 	size = size_lst(lst_a);
-	if (disorder < 20 || size <= 5)
+	if (disorder < 20)
 		return (simple(lst_a, lst_b, bench, size));
 	else if (20 <= disorder && disorder < 50)
 		return (medium(lst_a, lst_b, bench));
